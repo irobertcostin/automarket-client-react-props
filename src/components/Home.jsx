6491e95fa40ed088f14f437f9c75c335243512ad
irobertcostin <irobertcostin@gmail.com>
2023-03-31 11:07:01 +0300

    homepage update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,13 +9,15 @@ import Navbar from "./Navbar";
 
 export default function Home() {
 
-    
+
 
 
     return (
-        <>
-           
-        </>
+        <div className="test-drive-div flex flex-col items-center justify-end">
+            <div className="w-full test-drive h-[200px]"></div>
+
+
+        </div>
     )
 }
 
