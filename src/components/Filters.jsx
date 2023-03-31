d3faa43b85cf46e481caa956856aee9b7213127c
irobertcostin <irobertcostin@gmail.com>
2023-03-31 20:41:07 +0300

    some updates

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,13 +1,86 @@
 import Home from "./Home";
-import React from "react";
+import React,{useState,useEffect} from "react";
+import Data from "../services/Api";
 
-export default function Filters(){
+import { Cascader } from 'antd';
 
 
 
-    return(
-        <>
-            
-        </>
-    )
-}
+
+
+
+
+
+
+
+export default function Filters() {
+
+
+
+    let [makers, setMakers] = useState([]);
+    let [makerCount,setMakerCount]=useState();
+
+    let api = new Data();
+
+
+    let getMakers = async () => {
+        let response = await api.getMakers();
+        
+        setMakers(response)
+        setMakerCount(response.length)
+        console.log(makers)
+        console.log(makerCount)
+    }
+    
+
+    useEffect(() => {
+
+        
+        getMakers();
+        
+    }, [])
+
+
+    const optionLists = [
+       ,
+        {
+            value: 'BMW',
+            label: 'BMW',
+            isLeaf: false,
+        },
+    ];
+
+
+
+
+    const [options, setOptions] = useState(optionLists);
+
+    const onChange = (value, selectedOptions) => {
+        console.log(value, selectedOptions);
+    };
+
+
+    const loadData = (selectedOptions) => {
+        const targetOption = selectedOptions[selectedOptions.length - 1];
+        targetOption.loading = true;
+
+
+
+        // load options lazily
+        setTimeout(() => {
+            targetOption.loading = false;
+            targetOption.children = [
+                {
+                    label: `${targetOption.label} Dynamic 1`,
+                    value: 'dynamic1',
+                },
+                {
+                    label: `${targetOption.label} Dynamic 2`,
+                    value: 'dynamic2',
+                },
+            ];
+            setOptions([...options]);
+        }, 1000);
+    };
+    return <Cascader options={options} size="medium" loadData={loadData} onChange={onChange} changeOnSelect defaultValue={"Search"} />;
+};
